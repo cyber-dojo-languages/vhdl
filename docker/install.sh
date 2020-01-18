fa800036178e985cfad4ebd162b2f9ed89637710
Jon Jagger <jon@jaggersoft.com>
2020-01-18 21:19:50 +0000

    Switch to circleci

---
+++ docker/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -Eeu
+
+apt-get update
+apt-get install --no-install-recommends --yes git gnat libz-dev
+git config --global http.sslVerify false
+git clone "https://github.com/ghdl/ghdl.git" ghdl 
+cd ghdl
+./configure --prefix=/usr/local
+make
+make install
