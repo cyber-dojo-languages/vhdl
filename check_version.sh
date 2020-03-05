f7b86adcea64688fd9842bd677e0a803486a0246
Jon Jagger <jon@jaggersoft.com>
2020-03-05 03:20:14 +0000

    Now at v0.37

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=0.36
+readonly EXPECTED=0.37
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'ghdl --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
