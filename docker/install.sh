e36f29563aa913ef5ec38ba64b745fc1d6f80144
Jon Jagger <jon@jaggersoft.com>
2022-06-25 10:10:51 +0100

    Upgrade to v3.0.0

--- docker/install.sh
+++ docker/install.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install --no-install-recommends --yes git gnat libz-dev
 git config --global http.sslVerify false
-git clone "https://github.com/ghdl/ghdl.git" ghdl 
+git clone "https://github.com/ghdl/ghdl.git" ghdl
 cd ghdl
 ./configure --prefix=/usr/local
 make
